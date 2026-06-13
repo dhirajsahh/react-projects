869dbda97fc7724fda29e4fafc8609ed19254569
dhiraj <dhirajmahato2019@gmail.com>
2026-06-13 10:38:35 +0545

    multiple accordion

--- src/components/accrediation/index.jsx
+++ src/components/accrediation/index.jsx
@@ -4,24 +4,49 @@ import "./style.css";
 
 const Index = () => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [multipleItems, setMultipleItems] = useState([]);
+  const [showMultipleItems, setShowMultipleItems] = useState(false);
   const handleClick = (id) => {
     setSelectedItem(selectedItem === id ? null : id);
-   
   };
+  function handleMultipleItems(id) {
+    let arr = [...multipleItems];
+    const findIndexofid = arr.indexOf(id);
+    if (findIndexofid == -1) {
+      arr.push(id);
+    } else arr.splice(findIndexofid, 1);
+    setMultipleItems(arr);
+  }
+
   return (
     <div className="container">
+      <button onClick={() => setShowMultipleItems(!showMultipleItems)}>
+        show multiple accrediation
+      </button>
       {data?.map((dataItem) => {
         return (
-          <div>
-            <div onClick={() => handleClick(dataItem.id)} className="items">
+          <div key={dataItem.id}>
+            <div
+              onClick={() =>
+                showMultipleItems
+                  ? handleMultipleItems(dataItem.id)
+                  : handleClick(dataItem.id)
+              }
+              className="items"
+            >
               <h3>{dataItem.question}</h3>
               <span>+</span>
             </div>
-            <div>
+            {showMultipleItems
+              ? multipleItems.indexOf(dataItem.id) !== -1 && (
+                  <div>{dataItem.answer}</div>
+                )
+              : selectedItem === dataItem.id && <div>{dataItem.answer}</div>}
+            {/* <div onClick={() => setSelectedItem(null)}>
               {selectedItem === dataItem.id ? (
                 <div>{dataItem.answer}</div>
               ) : null}
-            </div>
+            </div> */}
           </div>
         );
       })}
